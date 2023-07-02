4d1a15ad3009d646e0e3e09820b6f29831892e64
kushagra1212 <kushagrarathore002@gmail.com>
2023-07-02 13:55:03 +0530

    add: store to localstorage

--- content.js
+++ content.js
@@ -1,8 +1,58 @@
-console.log('Hello from cnt');
-
-// Function to be executed when the DOM is fully loaded
 function onDOMLoaded() {
   const targetElement = document.querySelector('#editor');
+  let currentTime = new Date(); // Total time spent on the page
+  let changeCount = 0; // Counter for tracking the number of changes
+  let time = null; // Timer
+  let lang = null;
+
+  const startTimer = () => {
+    time = Date.now();
+    console.log('WakaTimer started');
+    const text = targetElement.textContent.trim();
+    lang = detectLanguage(text);
+    console.log('Language:', lang);
+  };
+
+  const stopTimer = () => {
+    const totalTime = Date.now() - time;
+    const totalTimeOnPage = Date.now() - currentTime;
+    time = null;
+    console.log('Timer stopped');
+    console.log('Result:', totalTime);
+    console.log('Language:', lang);
+
+    const wakaTime = 'wakatime'; // Replace with your WakaTime key
+    const storedData = localStorage.getItem(wakaTime);
+    let data = storedData ? JSON.parse(storedData) : [];
+    const currentDate = new Date();
+    data.push({
+      dateTime: currentDate,
+      totalTime,
+      totalTimeOnPage,
+      language: lang,
+    });
+    localStorage.setItem(wakaTime, JSON.stringify(data));
+  };
+
+  const detectLanguage = (text) => {
+    const lowercaseText = text.toLowerCase();
+
+    if (lowercaseText.includes('javascript')) {
+      return 'JavaScript';
+    } else if (lowercaseText.includes('java')) {
+      return 'Java';
+    } else if (lowercaseText.includes('python')) {
+      return 'Python';
+    } else if (lowercaseText.includes('c++')) {
+      return 'C++';
+    } else if (lowercaseText.includes('c')) {
+      return 'C';
+    }
+    // Add more conditions for other languages
+
+    return 'Unknown';
+  };
+
   const observer = new MutationObserver((mutationsList) => {
     for (let mutation of mutationsList) {
       if (mutation.type === 'childList' || mutation.type === 'characterData') {
@@ -13,12 +63,19 @@ function onDOMLoaded() {
         const changedText = Array.from(changedElements).map((element) =>
           element.textContent.trim()
         );
+
         if (changedText.length > 0) {
-          console.log('Content has changed:', changedText);
+          changeCount++;
+
+          if (changeCount === 1 && time === null) {
+            startTimer();
+          }
         }
       }
     }
   });
+
+  window.addEventListener('beforeunload', stopTimer);
   observer.observe(targetElement, {
     childList: true,
     characterData: true,
@@ -30,5 +87,5 @@ function onDOMLoaded() {
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', onDOMLoaded);
 } else {
-  onDOMLoaded();
+  setTimeout(() => onDOMLoaded(), 10000);
 }
